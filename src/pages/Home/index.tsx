bc4e5ee9e631cb21156eef3ba41b173896a38f80
your-username <toannvm@gofiber.vn>
2025-04-05 14:40:12 +0700

    hehe

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
 
   return (
     <>
-      <h1 className="text-[20px]">Minh toàn Heasdasdasdaasdasdassdshe</h1>
+      <h1 className="text-[20px]">Minh toàn Heasdasdasdaasdasasdasdasdassdshe</h1>
       <ComponentTable />
       <div>
     </div>
