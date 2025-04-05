eded38fc8e3e58de5f74cd6a234027e559bd7bc7
your-username <toannvm@gofiber.vn>
2025-04-05 10:17:22 +0700

    demo

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "./index.css"
+import "./index.css";
+import "antd/dist/reset.css";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
